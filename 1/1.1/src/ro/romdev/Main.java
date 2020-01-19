a5431c3df58ada16934d3017c14bbf7d32a150fa
andrei2802 <andreiv.alexandrescu@gmail.com>
2020-01-19 19:40:57 +0200

    tema1

--- 1/1.1/src/ro/romdev/Main.java
+++ 1/1.1/src/ro/romdev/Main.java
@@ -7,6 +7,23 @@ public class Main {
     }
 
     public static int myFunction(int x) {
-        return 0;
+        System.out.println(x);
+        int rez = -1;
+        if (x > 10 && x < 15) {
+            rez = 1;
+        }else if(x > 100 && x % 5 == 0){
+            rez = 10;
+        }else if(x > 15 && x % 2 == 0){
+            rez = 2;
+        }else if (x < 0){
+            rez = 0;
+        }else {
+
+            rez = 3;
+        }
+
+
+        System.out.println(rez);
+        return rez;
     }
 }
